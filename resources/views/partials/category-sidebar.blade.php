2034e89df923ab1cc989832f831dae236c6844c6
Cyrus Cuenca <cyrushcuenca@gmail.com>
2018-02-10 22:51:56 -0800

    Sidebar Revisions and Border Radius Fixes

--- resources/views/partials/category-sidebar.blade.php
+++ resources/views/partials/category-sidebar.blade.php
@@ -2,30 +2,30 @@
 	<div class="title-bar-thin">
 		<p>Categories</p>
 	</div>
-			<a href="/categories/metamorphic" style="display: block; margin-left: 7%; width: 86%; height: auto; border: 1px solid #ccc; margin-top: 8%; border-radius: 2px;">
+			<a href="/categories/metamorphic" style="display: block; margin-left: 10%; width: 80%; height: auto; border: 1px solid #ccc; margin-top: 40px; border-radius: 3px; color: grey;">
 				<img src="{{ asset('/images/metamorphic.jpg') }}" style="width: 100%;">
-				<p style="margin: 5px; padding-left: 8px;">
+				<p style="padding-left: 6%; margin-top: 9px;">
 					Metamorphic
 				</p>
 			</a>
 
-			<a href="/categories/sedimentary" style="display: block; margin-left: 7%; width: 86%; height: auto; border: 1px solid #ccc; margin-top: 8%; border-radius: 2px;">
+			<a href="/categories/sedimentary" style="display: block; margin-left: 10%; width: 80%; height: auto; border: 1px solid #ccc; margin-top: 24px; border-radius: 3px; color: grey;">
 				<img src="{{ asset('/images/sedimentary.jpg') }}" style="width: 100%;">
-				<p style="margin: 5px; padding-left: 8px;">
+				<p style="padding-left: 6%; margin-top: 9px;">
 					Sedimentary
 				</p>
 			</a>
 
-			<a href="/categories/igneous" style="display: block; margin-left: 7%; width: 86%; height: auto; border: 1px solid #ccc; margin-top: 8%; border-radius: 2px;">
+			<a href="/categories/igneous" style="display: block; margin-left: 10%; width: 80%; height: auto; border: 1px solid #ccc; margin-top: 24px; border-radius: 3px; color: grey;">
 				<img src="{{ asset('/images/igneous.jpg') }}" style="width: 100%;">
-				<p style="margin: 5px; padding-left: 8px;">
+				<p style="padding-left: 6%; margin-top: 9px;">
 					Igneous
 				</p>
 			</a>
 
-			<a href="/categories/mineral" style="display: block; margin-left: 7%; width: 86%; height: auto; border: 1px solid #ccc; margin-top: 8%; border-radius: 2px;">
+			<a href="/categories/mineral" style="display: block; margin-left: 10%; width: 80%; height: auto; border: 1px solid #ccc; margin-top: 24px; border-radius: 3px; color: grey;">
 				<img src="{{ asset('/images/mineral.jpg') }}" style="width: 100%;">
-				<p style="margin: 5px; padding-left: 8px;">
+				<p style="padding-left: 6%; margin-top: 9px;">
 					Mineral
 				</p>
 			</a>
