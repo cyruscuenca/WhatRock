c70577e55221d43ec2621257ace767c2aa1c4bf4
Cyrus Cuenca <cyrushcuenca@gmail.com>
2018-03-03 19:36:01 -0800

    Footer Update
    Footer is updated with a three column link list.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,27 @@
 <div style="background: #263238; width: 120%; margin-left: -10%; height: 50pt; border-top-right-radius: 50% 50pt; border-top-left-radius: 50% 50pt;  ">
 </div>
+<style type="text/css">
+	ul { margin: 0; padding: 0; }
+</style>
 <div style="width: 100%; background: #263238; height: auto; margin-bottom: none;">
 		<img src="{{ asset('images/map.svg') }}" style="width: 100%; display: block; margin-left: auto; margin-right: auto;">
+	<div style="width: 70%; margin-left: 15%; font-family: 'ubuntu'; font-size: 14pt; color: #CFD8DC; text-align: center; position: absolute; margin-top: -21%; height: 90pt;">
+		<ul style="height: 100%; display: inline-block; position: relative; width: 100pt; float: left; text-align: left; margin-left: 1.5pt;">
+			<li style="font-size: 17pt; color: #fff;">Help</li>
+			<li></li>
+			<li></li>
+		</ul>
+		<ul style="height: 100%; display: inline-block; position: relative; width: 100pt; margin: 0 auto; text-align: left;">
+			<li style="font-size: 17pt; color: #fff;">Contact</li>
+			<li></li>
+			<li></li>
+		</ul>
+
+		<ul style="height: 100%; display: inline-block; position: relative; float: right; width: 80pt; text-align: left; margin-left: 20pt;">
+			<li style="font-size: 17pt; color: #fff;">Partners</li>
+			<li></li>
+			<li></li>
+		</ul>
+
+	</div>
 </div>
