99391fbc95d43dfba8ac4889915111916e8bcd59
Cyrus Cuenca <cyrushcuenca@gmail.com>
2018-09-08 19:36:10 -0700

    Added Functions for New Views

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,30 +7,39 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function about(){
-    	return view('pages/about');
+    	return view('pages.about');
     }
     public function partners(){
-    	return view('pages/partners');
+    	return view('pages.partners');
     }
     public function contribute(){
-    	return view('pages/contribute');
+    	return view('pages.contribute');
     }
-    public function developers(){
-    	return view('pages/developers');
+    public function terms(){
+    	return view('pages.terms');
+    }
+    public function news(){
+        return view('pages.news');
+    }
+    public function earthquakes(){
+        return view('pages.earthquakes');
+    }
+    public function maps(){
+        return view('pages.maps');
     }
     public function index(){
         return view('index');
     }
     public function properties(){
-        return view('/learn/properties/index');
+        return view('learn.properties.index');
     }
     public function hardness(){
-    	return view('learn/properties/hardness');
+    	return view('learn.properties.hardness');
     }
     public function breakage(){
-        return view('learn/properties/breakage');
+        return view('learn.properties.breakage');
     }
     public function cleavage(){
-        return view('learn/properties/cleavage');
+        return view('learn.properties.cleavage');
     }
 }
